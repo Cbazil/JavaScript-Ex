55e452e61848c3a6ffe8583e3f487682df5de3d4
Cbazil <chadbosch08@gmail.com>
2018-11-07 10:27:07 +0200

    Added comments and did simple exercises

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+'use strict'; //The "use strict" directive switches the engine to the "modern" mode, changing the bahavior of some built-in features
+
+// Variables are used to store this information.
+var message;
+
+var massage = 'Good morning';
+    // message='How are you today?';
+alert(message);
+
+// var name ='Chad';
+//      age= 20;
+//      gender='male';
+// console.log(name);
+
+
+/* multi line comments
+*/
+
+// *****************************************************
+// *******************************************************
+
+// Always remember to close a string with the same type of quotation marks you starte it with.
+
+// Operators: 
+
+// + addition
+// - subtraction
+// / division
+// * multiplication
+
+// the + operator can be applied to two strings values. This will join two strings together, otherwise called concatenation.
+
+
+// In the console "Hey" + " there"
+
+
