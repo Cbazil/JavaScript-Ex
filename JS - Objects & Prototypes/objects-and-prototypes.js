90c4c1d43adc5acdbf9fdd77b01a2f6ac4d73ce6
Cbazil <chadbosch08@gmail.com>
2018-11-21 11:11:05 +0200

    Made two variables and creates objects asignned to them to call them when ever needed

--- JS - Objects & Prototypes/objects-and-prototypes.js
+++ JS - Objects & Prototypes/objects-and-prototypes.js
@@ -15,3 +15,34 @@ console.log(anotherObject.b); // will be undefined
 ========= End of 1st Activity */
 
 // Cake prototype
+
+var Pastry = {
+    type: "",
+    flavour: "",
+    layers: 0,
+    price: "",
+    occasion: "",
+
+
+describe : function() {
+    var description = "The " + this.type + " is a " + this.occasion + " pastry, has a " + this.flavour + " flavour, " + this.layers + " layer(s), and cost " + this.price + ".";
+    return description;
+    }
+};
+var muffin = Object.create(Pastry);
+muffin.type = "muffin";
+muffin.flavour = "blueberry";
+muffin.layers = "1";
+muffin.price = "R20";
+muffin.occasion = "wedding";
+
+var cake = Object.create(Pastry);
+cake.type = "cake";
+cake.flavour = "vanilla";
+cake.layers = "3";
+cake.price = "R100";
+cake.occasion = "birthday";
+
+console.log(muffin.describe());
+console.log(cake.describe());
+
