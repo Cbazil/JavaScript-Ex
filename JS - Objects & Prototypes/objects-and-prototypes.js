bf7eeaaf1b207be2bce3e389a1f6c42372e53fb2
Cbazil <chadbosch08@gmail.com>
2018-11-21 10:42:44 +0200

    Completed 1st activity, Creating first protype(multiple objects))

--- JS - Objects & Prototypes/objects-and-prototypes.js
+++ JS - Objects & Prototypes/objects-and-prototypes.js
@@ -1,11 +1,17 @@
 
-
+/* ========= Start of 1st activity
 var anObject = {
-    a: 2
+    a: 2,
+    b: 4
 };
 
 
 // Create anotherObject using anObject as a prototype
 var anotherObject = Object.create(anObject);
 
-console.log(anotherObject.a) // will show 2
+console.log(anotherObject.a); // will show 2
+console.log(anotherObject.b); // will be undefined
+
+========= End of 1st Activity */
+
+// Cake prototype
