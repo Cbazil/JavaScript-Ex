8be1d6bc1d8688cdb0ea39551ecaf369fbbad9c5
Cbazil <chadbosch08@gmail.com>
2018-11-27 11:21:19 +0200

    Made another function called wait with in the prototype method

--- JS - Objects & Prototypes/JS - Exercise (Pizza)/objects_and_prototypes.js
+++ JS - Objects & Prototypes/JS - Exercise (Pizza)/objects_and_prototypes.js
@@ -1,4 +1,5 @@
 var Pizza = {
+    // Method init
     init: function(type, base, layers, toppings, price){
         this.type = type;
         this.base = base;
@@ -12,7 +13,14 @@ var Pizza = {
     describe: function(){
         var description = "The " + this.type + " pizza has a " + this.base + " base with " + this.layers + " layers topped with " + this.toppings + " and all this will cost you.. " + this.price + ".";
         return description;
+    },
+    
+    /* ======== How long it takes to make pizza | Start ========= */
+    wait: function(){
+        var time = this.type + " pizza was added to the oven and it will take 7 minutes.";
+        return time;
     }
+    /* ======== How long it takes to make pizza | End ========= */
 };
 
 var vegaterean = Object.create(Pizza);
@@ -24,8 +32,14 @@ halaal.init("halaal", "thin", 3, "chicken, cheese plus mushrooms", "R180");
 var meaty = Object.create(Pizza);
 meaty.init("meaty", "thin", 2, "bacon plus cheese", "R180");
 
-console.log(halaal.describe());
-console.log(meaty.describe());
-console.log(meaty.describe());
+console.log(halaal.describe()+ ".." + halaal.wait());
+console.log(meaty.describe() + ".." + meaty.wait());
+console.log(vegaterean.describe() + ".." + vegaterean.wait());
+
+
+
+
+
+
 
 // <=============== 2018/11/27 ===============
