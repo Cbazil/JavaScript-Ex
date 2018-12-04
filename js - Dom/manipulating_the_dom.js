9c7bc8b2b1ff1fe420a9474b73bc7a2f31f6675d
Cbazil <chadbosch08@gmail.com>
2018-12-04 09:45:17 +0200

    Lecture on DOM - Writing js code for document class/IDs

---
+++ js - Dom/manipulating_the_dom.js
@@ -0,0 +1,4 @@
+//  modify documents after they have finished loading-- add new content to a page, change the color of an element to reflect a changing 
+
+
+// Send and receive data to and from external APIs- harness the power of the thousands of useful
