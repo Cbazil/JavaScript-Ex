3eae73af0bc8303fc3a460971b3edcc36edb5c1b
Cbazil <chadbosch08@gmail.com>
2018-12-04 10:33:59 +0200

    Made color button that change paragraph

--- js - Dom/dom.js
+++ js - Dom/dom.js
@@ -1,1 +1,16 @@
-document.getElementById("demo").innerHTML = "Hello World!";
+// document.getElementById("demo").innerHTML = "DOM lets you write or change content within your html by targeting elements";
+// Creat a paragraph that describe what dom used for.
+// Create two buttons below paragraph
+// Buttons Blue and Red 
+// Blue should change to Red
+// Red should change to blue
+
+// document.getElementById("blue").style.backgroundColor = "blue";
+// document.getElementById("red").style.backgroundColor = "red";
+// document.getElementById("blue").innerHTML = "Blue Button";
+// document.getElementById("red").innerHTML = "Red Button";
+
+function changeColor(newColor){
+    let btn = document.getElementById('text');
+    btn.style.color = newColor;
+} 
